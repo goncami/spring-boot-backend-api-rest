fa3fa39d3cb0ee6146df11aba4de414aa097f913
gon <g.casmi@gmail.com>
2020-09-06 19:10:05 +0200

    nombre not null email not null, unique

--- src/main/java/com/prueba/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/prueba/springboot/backend/apirest/models/entity/Cliente.java
@@ -3,14 +3,7 @@ package com.prueba.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 @Entity
 @Table(name="clientes")
@@ -19,14 +12,21 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(nullable = false)
 	private String nombre;
 	private String apellido;
+	@Column(nullable = false, unique = true)
 	private String email;
 	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+
+	@PrePersist
+	public void prePersist(){
+		createAt = new Date();
+	}
 
 	public Long getId() {
 		return id;
