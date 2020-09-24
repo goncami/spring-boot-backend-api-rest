7a070af5c118f84a9413d47ce52587f8661640ca
gon <g.casmi@gmail.com>
2020-09-24 21:03:24 +0200

    metodo findAll paginado

--- src/main/java/com/prueba/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/prueba/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,9 +1,8 @@
 package com.prueba.springboot.backend.apirest.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.prueba.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
 }
--- src/main/java/com/prueba/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/prueba/springboot/backend/apirest/models/services/IClienteService.java
@@ -3,10 +3,14 @@ package com.prueba.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import com.prueba.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+
+	public Page<Cliente> findAll(Pageable pageable);
 
 	public Cliente save(Cliente cliente);
 
